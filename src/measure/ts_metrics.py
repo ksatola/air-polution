b34b4f6f02f4f220fc34294073a8ee5d720e8ec5
ksatola <krzysztof@satola.net>
2020-02-29 18:47:15 +0100

    FEAT: MAE function

--- src/measure/ts_metrics.py
+++ src/measure/ts_metrics.py
@@ -11,5 +11,15 @@ def get_rmse(observed: pd.Series, predicted: pd.Series) -> np.float:
     :return: RMSE value
     """
     mse = mean_squared_error(observed, predicted)
-    rmse = np.sqrt(mse)
+    rmse = round(np.sqrt(mse), 4)
     return rmse
+
+
+def get_mae(observed: pd.Series, predicted: pd.Series) -> np.float:
+    """
+    Calculates MAE - Mean Absolute Error between two series
+    :param observed: pandas series with observed values (the truth)
+    :param predicted: pandas series with predicted values
+    :return: MAE value
+    """
+    return round(abs(observed.pct_change() - predicted.pct_change()).mean(), 4)
