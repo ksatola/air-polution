3c394b450161cfdb522dc6ab523e40873219b297
ksatola <krzysztof@satola.net>
2020-03-01 12:19:03 +0100

    FEAT: Hit rate function

--- src/measure/__init__.py
+++ src/measure/__init__.py
@@ -1,5 +1,7 @@
 """This package is dedicated for preparation of dataset for the analysis and modeling."""
 from measure.ts_metrics import (
     get_rmse,
+    get_mae,
+    get_hit_rate,
 )
 
--- src/measure/ts_metrics.py
+++ src/measure/ts_metrics.py
@@ -17,9 +17,29 @@ def get_rmse(observed: pd.Series, predicted: pd.Series) -> np.float:
 
 def get_mae(observed: pd.Series, predicted: pd.Series) -> np.float:
     """
-    Calculates MAE - Mean Absolute Error between two series
+    Calculates MAE - Mean Absolute Error between two series as percentage change
     :param observed: pandas series with observed values (the truth)
     :param predicted: pandas series with predicted values
     :return: MAE value
     """
     return round(abs(observed.pct_change() - predicted.pct_change()).mean(), 4)
+
+
+def get_hit_rate(observed: pd.Series, predicted: pd.Series) -> np.float:
+    """
+    Calculates "hit rate" between two series. The hit is defined as the same decision regarding
+    prediction direction comparing to observed direction (time series next point going up or down).
+    :param observed: pandas series with observed values (the truth)
+    :param predicted: pandas series with predicted values
+    :return: MAE value
+    """
+    # Build a data frame
+    pred_pct = pd.concat([observed.pct_change(), predicted.pct_change()], axis=1)
+    pred_pct.dropna(inplace=True)
+    pred_pct.columns = ['Observed', 'Predicted']
+
+    # Mark rows with a move in the same direction
+    pred_pct['Hit'] = np.where(np.sign(pred_pct['Observed']) == np.sign(pred_pct['Predicted']), 1, 0)
+
+    # Returm the hit rate
+    return round((pred_pct['Hit'].sum() / pred_pct['Hit'].count()) * 100, 2)
