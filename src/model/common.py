a7d0c1aa9fbfb30813ab4d7f09a4f50293f5763b
ksatola <krzysztof@satola.net>
2020-05-29 21:44:42 +0200

    FEAT: super learner

--- src/model/common.py
+++ src/model/common.py
@@ -1,4 +1,13 @@
 import pandas as pd
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.ensemble import BaggingRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import ElasticNet
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
 
 from logger import logger
 
@@ -13,3 +22,20 @@ def load_data(data_file_path: str) -> None:
     logger.info(f'DataFrame size: {df.shape}')
     display(df.head())
     return df
+
+
+def get_ensemble_models_for_regression() -> list:
+    """
+    Defines a list of regression models to be used in ensemble modelling.
+    :return: list of model
+    """
+    models = [LinearRegression(),
+              ElasticNet(),
+              SVR(gamma='scale'),
+              DecisionTreeRegressor(),
+              KNeighborsRegressor(),
+              AdaBoostRegressor(),
+              BaggingRegressor(n_estimators=10),
+              RandomForestRegressor(n_estimators=10),
+              ExtraTreesRegressor(n_estimators=10)]
+    return models
